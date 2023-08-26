844f5f5561835a115d0fc0e0e20989407aedc409
s23sean321 <141469984+s23sean321@users.noreply.github.com>
2023-08-26 15:29:42 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from models.database import db_session,init_db
 from models.user import Users
 
 from models.product import Products
+from models.cart import Cart
 
 app = Flask(__name__)
 
@@ -95,7 +96,7 @@ def callback():
 def handle_message(event):
 
     get_or_create_user(event.source.user_id)
-
+    cart = Cart(user_id=event.source.user_id)
 
     message_text = str(event.message.text).lower()
 
@@ -103,6 +104,31 @@ def handle_message(event):
         about_us_event(event)
     elif message_text == '我想訂購商品':
         message = Products.list_all()
+    elif "I'd like to have:" in message_text:
+            product_name=message_text.split(',')[0]
+            num_item = message_text.rsplit(':')[1]
+            product = db_session.query(Products).filter(Products.name.ilike(product_name)).first()
+            if product:
+
+                cart.add(product=product_name, num=num_item)
+                #然後利用confirm_template的格式詢問用戶是否還要加入？
+                confirm_template = ConfirmTemplate(
+                    text='Sure, {} {}, anything else?'.format(num_item, product_name),
+                    actions=[
+                        MessageAction(label='Add', text='add'),
+                        MessageAction(label="That's it", text="That's it")
+                    ])
+
+                message = TemplateSendMessage(alt_text='anything else?', template=confirm_template)
+            else:
+                message = TextSendMessage(text= "Sorry ,We don't havev {}.".format(product_name))
+            print(cart.bucket())
+    elif message_text in ['my cart','cart',"that's it"]:
+
+        if cart.bucket():
+            message = cart.display()
+        else:
+            message =TextSendMessage(text='your cart is empty now')
     if message:
         line_bot_api.reply_message(
             event.reply_token,message
